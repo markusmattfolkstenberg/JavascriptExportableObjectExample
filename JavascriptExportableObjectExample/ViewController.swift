bdae321f49f09d483e90923c789315ec6e643b0e
Markus Stenberg <markus.stenberg@uptive.se>
2020-04-17 08:48:29 +0200

    Initial commit.

---
+++ JavascriptExportableObjectExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JavascriptExportableObjectExample
+//
+//  Created by Markus on 2020-04-17.
+//  Copyright © 2020 phoz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
